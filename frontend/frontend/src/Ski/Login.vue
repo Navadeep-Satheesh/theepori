e5a4804117e8119213f062731d562d25ac767078
meenakshysudhir <ukms2004@gmail.com>
2024-05-05 01:22:46 +0530

    we

--- frontend/frontend/src/Ski/Login.vue
+++ frontend/frontend/src/Ski/Login.vue
@@ -1,22 +1,4 @@
 <template>
-  <!-- <div class="row">
-        <div class="col-md-6 offset-md-3"><form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form></div>
-    </div> -->
   <div class="top-container">
     <div class="container">
       <p class="heading">Login !</p>
@@ -68,7 +50,7 @@ export default {
 
       console.log("User doesnot exist or incorrect password");
     }
-  } catch (error) {
+  } catch(error) {
     // Handle errors (e.g., network error)
     console.error('An error occurred:', error);
     },
