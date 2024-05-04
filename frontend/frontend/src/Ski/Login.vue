01a5a28ccca755859c061959302b145bff6aefe3
meenakshysudhir <ukms2004@gmail.com>
2024-05-05 00:54:29 +0530

    hello

--- frontend/frontend/src/Ski/Login.vue
+++ frontend/frontend/src/Ski/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row">
+  <!-- <div class="row">
         <div class="col-md-6 offset-md-3"><form>
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
@@ -16,9 +16,93 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form></div>
+    </div> -->
+  <div class="top-container">
+    <div class="container">
+      <p class="heading">Login !</p>
+      <div class="login-container">
+        <form>
+          <label class="labels">Email</label>
+          <div class="input-box">
+            <input type="email" placeholder="Email" required v-model="email" />
+          </div>
+          <label class="labels">Password</label>
+          <div class="input-box">
+            <input
+              type="password"
+              placeholder="Password"
+              required
+              v-model="pwd"
+            />
+          </div>
+          <button class="submit-btn" type="submit" v-on:click="tryUser()">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-    
+    <p>password : {{ pwd }}</p>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: "",
+      pwd: "",
+    };
+  },
+  methods: {
+    asynch tryUser() {
+      let result = await axios.post("\api",
+        {
+            type: "user",
+            email:this.email,
+            password:this.pwd;
+        }
+      );
+      console.warn(result);
+    },
+  },
+};
 </script>
+<style scoped>
+.top-container {
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.heading {
+  padding: 20px;
+  font-size: 30px;
+}
+.container {
+  min-height: 50vh;
+  min-width: 20vw;
+  background-color: aliceblue;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login-container {
+  display: flex;
+  flex-direction: column;
+}
+.input-box {
+  padding: 20px;
+}
+.labels {
+  padding: 20px;
+}
+.input {
+  min-height: 6vh;
+}
+.submit-btn {
+  width: 10vw;
+  height: 3vh;
+  margin: 18px;
+  border-radius: 10px;
+}
+</style>
